cdbe7211f9223585fdc8df221735a8237bc54c53
MokaNiax <schmitzsemalvalentin@gmail.com>
2026-03-03 19:24:14 +0100

    ADDED: comera rotation with Q and E keys

--- Scripts/Player/camera.gd
+++ Scripts/Player/camera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 @export var direction_speed: float = 600.0
+@export var rotation_speed: float = 100.0
 @export var zoom_speed: float = 0.1
 
 func _process(delta: float) -> void:
@@ -18,7 +19,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 
+	if Input.is_action_pressed("turn left"):
+		rotation_degrees -= rotation_speed * delta
+	if Input.is_action_pressed("turn right"):
+		rotation_degrees += rotation_speed * delta
+
 	if direction != Vector2.ZERO:
+		direction = direction.rotated(rotation)
 		position += direction.normalized() * direction_speed * delta
 
 	if Input.is_action_just_released("scroll up") and zoom < Vector2(4, 4):
